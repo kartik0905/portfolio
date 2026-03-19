00b462f89d2d6ec7f68fa79dad44e7641b9b9735
Kartik Garg <kartikamitgarg2005@gmail.com>
2026-03-19 21:28:09 +0530

    Fix GSAP trial and update to standard free version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 import HoverLinks from "./HoverLinks";
-import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import gsap from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 export let smoother: ScrollSmoother;
 
 const Navbar = () => {
